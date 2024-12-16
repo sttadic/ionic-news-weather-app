38d8dae692548eb637416b8381f8ba5523141df7
sttadic <stjepan.tadich@gmail.com>
2024-12-16 14:03:11 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'G00438839',
+  webDir: 'www'
+};
+
+export default config;
