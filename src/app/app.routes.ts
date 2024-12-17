86097d716b2d8d566a1dc5743c547b425c989683
sttadic <stjepan.tadich@gmail.com>
2024-12-17 18:36:01 +0000

    Create Countries page and MyHttp service. Implement get method in a service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,4 +14,8 @@ export const routes: Routes = [
     path: 'settings',
     loadComponent: () => import('./settings/settings.page').then( m => m.SettingsPage)
   },
+  {
+    path: 'countries',
+    loadComponent: () => import('./countries/countries.page').then( m => m.CountriesPage)
+  },
 ];
