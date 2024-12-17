62a7943db4bb4e08d2c0b176aad5ef3c851ab1a3
sttadic <stjepan.tadich@gmail.com>
2024-12-17 12:42:18 +0000

    Implement logic to store country search argument to ionic storage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,8 @@ import { FormsModule } from '@angular/forms';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonInput, IonButton } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { settingsOutline } from 'ionicons/icons';
+import { StorageService } from '../services/storage.service';
+import { count } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +16,11 @@ import { settingsOutline } from 'ionicons/icons';
 export class HomePage {
   country!: string;
 
-  constructor() {
+  constructor(private ss: StorageService) {
     addIcons({settingsOutline});
+  }
+
+  async storeCountry() {
+    await this.ss.set("country", this.country);
   }
 }
